56cd8eeb10dc45603c5ce561b6f726d2fbe2d476
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2019-03-04 14:45:25 +0100

    Add workers ssupport

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use std::env;
 use std::io::{self, Read, Write};
 use std::net::{Shutdown, SocketAddr};
 use std::sync::{Arc, Mutex};
+use std::thread;
 use std::process::exit;
 use tokio::io::{copy, shutdown};
 use tokio::net::{TcpListener, TcpStream};
@@ -42,12 +43,56 @@ fn listener(addr: &SocketAddr) -> io::Result<TcpListener> {
     })
 }
 
+fn serve(addr: SocketAddr, urls: Vec<std::net::SocketAddr>, w: u16) -> io::Result<()> {
+  let worker = w.clone();
+  let sock = listener(&addr)?;
+  let done = sock
+    .incoming()
+    .map_err(move |e| error!("[worker-{}] Error accepting socket; error = {:?}", worker, e))
+    .for_each(move |client| {
+        let index = rand::thread_rng().gen_range(0, urls.len());
+        let url = &urls[index];
+        let server = TcpStream::connect(&url);
+        let amounts = server.and_then(move |server| {
+            let client_reader = MyTcpStream(Arc::new(Mutex::new(client)));
+            let client_writer = client_reader.clone();
+            let server_reader = MyTcpStream(Arc::new(Mutex::new(server)));
+            let server_writer = server_reader.clone();
+            let client_to_server = copy(client_reader, server_writer)
+                .and_then(|(n, _, server_writer)| shutdown(server_writer).map(move |_| n));
+
+            let server_to_client = copy(server_reader, client_writer)
+                .and_then(|(n, _, client_writer)| shutdown(client_writer).map(move |_| n));
+
+            client_to_server.join(server_to_client)
+        });
+
+        let msg = amounts
+            .map(move |(from_client, from_server)| {
+                debug!(
+                    "[worker-{}] client wrote {} bytes and received {} bytes",
+                    worker, from_client, from_server
+                );
+            })
+            .map_err(move |e| {
+                debug!("[worker-{}] Error in amounts, don't panic maybe the client just disconnected too soon: {}", worker, e);
+            });
+
+        tokio::spawn(msg);
+
+        Ok(())
+    });
+  tokio::run(done);
+  Ok(())
+}
+
 fn run_proxy() -> Result<(), Box<std::error::Error>> {
 
     let mut urls: Vec<std::net::SocketAddr> = Vec::new();
     let mut servers: Vec<String> = Vec::new();
     let mut bind = "127.0.0.1:8000".to_string();
     let mut log_level = "info".to_string();
+    let mut workers = 1;
 
     {
         let mut ap = ArgumentParser::new();
@@ -62,6 +107,10 @@ fn run_proxy() -> Result<(), Box<std::error::Error>> {
         ap.refer(&mut log_level).add_option(&["-l", "--log"],
                                             Store,
                                             "Log level [debug, info, warn, error] (info)");
+                                      
+        ap.refer(&mut workers).add_option(&["-w", "--workers"],
+                                            Store,
+                                            "Number of workers");
 
         ap.parse_args_or_exit();
     }
@@ -70,7 +119,7 @@ fn run_proxy() -> Result<(), Box<std::error::Error>> {
     env_logger::init();
 
     if servers.is_empty() {
-        error!("Need at least one server to load balance");
+        error!("You need to specify at least one server to load balance");
         exit(1);
     }
     for url in servers {
@@ -86,54 +135,24 @@ fn run_proxy() -> Result<(), Box<std::error::Error>> {
       }
     }
 
-    let addr = bind.parse().unwrap();
-    //let sock = TcpListener::bind(&addr).unwrap();
-    let sock = listener(&addr)?;
-
+    let addr: SocketAddr = bind.parse().unwrap();
+    // let sock = TcpListener::bind(&addr).unwrap();
     info!("TCP load balancer listening on {}", bind);
     info!("forwarding traffic to");
     for url in urls.clone() {
       info!(" *  {}", url);
     }
-
-    let done = sock
-        .incoming()
-        .map_err(|e| println!("Error accepting socket; error = {:?}", e))
-        .for_each(move |client| {
-            let index = rand::thread_rng().gen_range(0, urls.len());
-            let url = &urls[index];
-            let server = TcpStream::connect(&url);
-            let amounts = server.and_then(move |server| {
-                let client_reader = MyTcpStream(Arc::new(Mutex::new(client)));
-                let client_writer = client_reader.clone();
-                let server_reader = MyTcpStream(Arc::new(Mutex::new(server)));
-                let server_writer = server_reader.clone();
-                let client_to_server = copy(client_reader, server_writer)
-                    .and_then(|(n, _, server_writer)| shutdown(server_writer).map(move |_| n));
-
-                let server_to_client = copy(server_reader, client_writer)
-                    .and_then(|(n, _, client_writer)| shutdown(client_writer).map(move |_| n));
-
-                client_to_server.join(server_to_client)
-            });
-
-            let msg = amounts
-                .map(move |(from_client, from_server)| {
-                    debug!(
-                        "client wrote {} bytes and received {} bytes",
-                        from_client, from_server
-                    );
-                })
-                .map_err(|e| {
-                    debug!("Error in amounts, don't panic maybe the client just disconnected too soon: {}", e);
-                });
-
-            tokio::spawn(msg);
-
-            Ok(())
-        });
-
-    tokio::run(done);
+    let threads = (0..workers).map(|i| {
+        info!("Starting worker {} ...", i);
+        let the_urls = urls.clone();
+        let the_addr = addr.clone();
+        thread::Builder::new().name(format!("worker-{}", i)).spawn(move || {
+            serve(the_addr, the_urls, i);
+        }).unwrap()
+    }).collect::<Vec<_>>();
+    for thread in threads {
+        thread.join().unwrap();
+    }
     Ok(())
 }
 
